c2720fbd4711973ca8425ac05a97550b1a2f7970
BRVallmi <118301519+BRVallmi@users.noreply.github.com>
2024-05-24 13:42:24 +0200

    fix again

--- panel/logout.php
+++ panel/logout.php
@@ -1,10 +1,10 @@
 <?php
-require "../lib/ssh/remotescript.php";
-session_start();
-startDocker($data['IPCurso'],$data['ipAlumno'],$user,$_SERVER["REMOTE_ADDR"],null);
-session_unset();
-session_destroy();
-setcookie('session', '', time() - 3600, '/');
-header('Location: ../index.php');
-exit();
+    require "../lib/ssh/remotescript.php";
+    session_start();
+    startDocker($_SESSION['IPCurso'],$_SESSION['ipAlumno'],$_SESSION['user'],$_SERVER["REMOTE_ADDR"],null);
+    session_unset();
+    session_destroy();
+    setcookie('session', '', time() - 3600, '/');
+    header('Location: ../index.php');
+    exit();
 ?>
