d83485d6844917d7d9cca2ef9842635bad45c873
BRVallmi <118301519+BRVallmi@users.noreply.github.com>
2024-05-24 13:47:26 +0200

    Fin

--- panel/logout.php
+++ panel/logout.php
@@ -1,7 +1,7 @@
 <?php
     require "../lib/ssh/remotescript.php";
     session_start();
-    startDocker($_SESSION['IPCurso'],$_SESSION['ipAlumno'],$_SESSION['user'],$_SERVER["REMOTE_ADDR"],null);
+    startDocker($_SESSION['course'],$_SESSION['alumno'],$_SESSION['user'],$_SERVER["REMOTE_ADDR"],null);
     session_unset();
     session_destroy();
     setcookie('session', '', time() - 3600, '/');
