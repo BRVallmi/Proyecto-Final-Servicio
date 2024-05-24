126154c5ed6c5effad8392349d0bcbed4aacd0f0
BRVallmi <118301519+BRVallmi@users.noreply.github.com>
2024-05-24 13:38:39 +0200

    Fix puta

--- panel/logout.php
+++ panel/logout.php
@@ -1,10 +1,10 @@
 <?php
 require "../lib/ssh/remotescript.php";
 session_start();
+startDocker($data['IPCurso'],$data['ipAlumno'],$user,$_SERVER["REMOTE_ADDR"],null);
 session_unset();
 session_destroy();
 setcookie('session', '', time() - 3600, '/');
-startDocker($data['IPCurso'],$data['ipAlumno'],$user,$_SERVER["REMOTE_ADDR"],null);
 header('Location: ../index.php');
 exit();
 ?>
